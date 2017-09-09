f98c53a5b4bb4141b833e1b660f50e6900790159
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-09-09 10:06:37 +0200

    moved

--- backend/api.js
+++ backend/api.js
@@ -2,10 +2,7 @@
  * Created by Vadym Yatsyuk on 08.09.17
  */
 
-const blynk = require('blynk-client');
 const Router = require('express').Router;
-
-const blynkClient = blynk.connect();
 
 let router = new Router();
 router.get('/', (req, res) => {
