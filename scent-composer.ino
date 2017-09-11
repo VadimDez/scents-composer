e60d3e341914d11b7ea28c6d8a1a05586908dfc5
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-09-11 22:31:25 +0200

    Update scent-composer.ino

--- scent-composer.ino
+++ scent-composer.ino
@@ -7,7 +7,7 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "39c668ad76694160a130e698c368fad7";
+char auth[] = "...";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
